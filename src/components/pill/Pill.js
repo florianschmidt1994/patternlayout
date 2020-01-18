f377f8d021462de02cb250431f437de344e8b75d
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-01-18 20:14:31 +0100

    Add close button to pills

--- src/components/pill/Pill.js
+++ src/components/pill/Pill.js
@@ -5,7 +5,9 @@ export default function ({conversionCharacter, description, closeable = false}) 
     return (
         <div className="pill">
             <div>{description} '{conversionCharacter}'</div>
-            <div className="close-button"/>
+            <div className="close-button">
+                <div className="close-cross"/>
+            </div>
         </div>
     );
 }
